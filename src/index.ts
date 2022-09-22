8cd404bfbd4339af73f473a90768075c2badb1bf
bduque <bduque@lean-tech.io>
2022-09-22 01:01:46 +0000

    feat(add line of code): line of code added

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,8 @@ const port = 3000;
 
 const server = http.createServer(app);
 
+console.log('adding some console log')
+
 server.listen(port, '0.0.0.0', () => {
     console.log(`Server Running on Port ${port}`);
   });
