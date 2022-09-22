e6c3fb12a057492ebb5c1ba15c62b87203f6a1aa
bduque <bduque@lean-tech.io>
2022-09-22 00:35:21 +0000

    feat(add router): router created and definitions of server

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,10 @@
 import http from 'http';
+import app from './app';
 
 const port = 3000;
 
-const server = http.createServer();
+const server = http.createServer(app);
+
 server.listen(port, '0.0.0.0', () => {
     console.log(`Server Running on Port ${port}`);
   });
